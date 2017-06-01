22228f5d7526f307aebac1a26011694a99ce0f67
Katrix- <katrix97@hotmail.com>
2017-06-01 23:36:27 +0200

    Update WSHandler
    No longer uses a publisher as a middle man (might get back to a better solution later)
    Better shutdown routine

--- akkaCord/src/main/scala/net/katsstuff/akkacord/http/websocket/WsHandler.scala
+++ akkaCord/src/main/scala/net/katsstuff/akkacord/http/websocket/WsHandler.scala
@@ -26,14 +26,12 @@ package net.katsstuff.akkacord.http.websocket
 import scala.concurrent.duration._
 import scala.language.postfixOps
 
-import akka.actor.{ActorRef, Cancellable, FSM, Props}
+import akka.actor.{ActorRef, Cancellable, FSM, Props, Status}
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.Uri.Query
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.model.ws.{InvalidUpgradeResponse, Message, TextMessage, ValidUpgrade}
 import akka.http.scaladsl.unmarshalling.Unmarshal
-import akka.stream.actor.ActorSubscriberMessage.{OnComplete, OnError, OnNext}
-import akka.stream.actor.{ActorSubscriber, OneByOneRequestStrategy}
 import akka.stream.scaladsl._
 import akka.stream.{ActorMaterializer, OverflowStrategy}
 import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport
@@ -42,11 +40,10 @@ import io.circe.{parser, _}
 import net.katsstuff.akkacord.DiscordClient.ShutdownClient
 import net.katsstuff.akkacord.http.Routes
 import net.katsstuff.akkacord.http.websocket.WsEvent.ReadyData
-import net.katsstuff.akkacord.{DiscordClientSettings, APIMessageHandlerEvent}
+import net.katsstuff.akkacord.{APIMessageHandlerEvent, DiscordClientSettings, Request}
 
 class WsHandler(token: String, cache: ActorRef, settings: DiscordClientSettings)
     extends FSM[WsHandler.State, WsHandler.Data]
-    with ActorSubscriber
     with FailFastCirceSupport {
   import WsHandler._
   import WsProtocol._
@@ -56,9 +53,6 @@ class WsHandler(token: String, cache: ActorRef, settings: DiscordClientSettings)
 
   import system.dispatcher
 
-  override protected def requestStrategy = OneByOneRequestStrategy
-
-  private var shouldShutDown       = false
   private var reconnectAttempts    = 0
   private val maxReconnectAttempts = settings.maxReconnectAttempts
 
@@ -67,34 +61,32 @@ class WsHandler(token: String, cache: ActorRef, settings: DiscordClientSettings)
   startWith(Inactive, WithResumeData(None))
 
   onTransition {
-    case Active -> Inactive => self ! TryToConnect
+    case Active -> Inactive     => self ! TryToConnect
+    case Active -> ShuttingDown => self ! ShutdownStart
   }
 
   override def preStart(): Unit = self ! TryToConnect
 
   when(Inactive) {
     case Event(TryToConnect, data) =>
-      data.sourceOpt.foreach(_.complete())
-      data.heartbeatCancelableOpt.foreach(_.cancel())
-
-      if (!shouldShutDown && reconnectAttempts < maxReconnectAttempts) {
+      if (reconnectAttempts < maxReconnectAttempts) {
+        data.heartbeatCancelableOpt.foreach(_.cancel())
+        data.sourceOpt.foreach(_.complete())
         reconnectAttempts += 1
 
         log.info("Trying to get gateway")
         Http()
           .singleRequest(HttpRequest(uri = Routes.gateway))
           .flatMap {
-            case HttpResponse(StatusCodes.OK, headers, entity, _) =>
-              log.debug(s"Got WS gateway.\nHeaders:\n${headers.mkString("\n")}\n Entity:$entity")
+            case HttpResponse(StatusCodes.OK, _, entity, _) =>
+              log.debug(s"Got WS gateway.")
               Unmarshal(entity).to[Json]
             case HttpResponse(code, headers, entity, _) =>
               entity.discardBytes()
-              throw new IllegalStateException(
-                s"Could not get WS gateway.\nStatusCode: ${code.value}\nHeaders:\n${headers.mkString("\n")}"
-              )
+              throw new IllegalStateException(s"Could not get WS gateway.\nStatusCode: ${code.value}\nHeaders:\n${headers.mkString("\n")}")
           }
           .foreach { js =>
-            js.hcursor.downField("url").as[String] match {
+            js.hcursor.get[String]("url") match {
               case Right(gateway) => self ! ReceivedGateway(gateway)
               case Left(e)        => throw e
             }
@@ -102,29 +94,26 @@ class WsHandler(token: String, cache: ActorRef, settings: DiscordClientSettings)
 
         stay using WithResumeData(data.resume)
       } else {
-        data.sourceOpt.foreach(_.watchCompletion().foreach { _ =>
-          mat.shutdown()
-        })
-        stop()
+        goto(ShuttingDown)
       }
     case Event(ReceivedGateway(uri), WithResumeData(resume)) =>
       reconnectAttempts = 0
 
       log.info(s"Got gateway: $uri")
-      val sourceQueue   = Source.queue[Message](64, OverflowStrategy.fail)
-      val destPublisher = Sink.asPublisher[Message](fanout = false)
+      val sourceQueue = Source.queue[Message](64, OverflowStrategy.fail)
+      val sink        = Sink.actorRef[Message](self, QueueCompleted)
 
-      val flow = Flow.fromSinkAndSourceMat(destPublisher, sourceQueue)(Keep.both)
+      val flow = Flow.fromSinkAndSourceMat(sink, sourceQueue)(Keep.right)
 
-      val (futureResponse, (dest, source)) = Http().singleWebSocketRequest(wsUri(uri), flow)
+      val (futureResponse, source) = Http().singleWebSocketRequest(wsUri(uri), flow)
 
       futureResponse.foreach {
         case InvalidUpgradeResponse(response, cause) =>
           response.discardEntityBytes()
+          source.complete()
           throw new IllegalStateException(s"Could not connect to gateway: $cause")
         case ValidUpgrade(response, _) =>
           response.discardEntityBytes()
-          dest.subscribe(ActorSubscriber(self)) //Is this safe to do in another thread?
           self ! ValidWsUpgrade
       }
 
@@ -132,14 +121,36 @@ class WsHandler(token: String, cache: ActorRef, settings: DiscordClientSettings)
     case Event(ValidWsUpgrade, _) => goto(Active)
   }
 
+  when(ShuttingDown) {
+    case Event(ShutdownStart, data) =>
+      log.info("Starting shut down")
+      data.heartbeatCancelableOpt.foreach(_.cancel())
+      data.sourceOpt.foreach { source =>
+        source.watchCompletion().foreach { _ =>
+          log.info("Queue completed")
+        }
+        source.complete()
+      }
+      stay()
+    case Event(QueueCompleted, _) =>
+      log.info("Queue completed message")
+      mat.shutdown()
+      self ! ShutdownFinish
+      stay()
+    case Event(ShutdownFinish, _) =>
+      log.info("Shutdown complete")
+      stop()
+  }
+
   when(Active) {
-    case Event(OnComplete, _) =>
+    case Event(QueueCompleted, _) =>
       log.info("Websocket connection completed")
       goto(Inactive)
-    case Event(OnError(e), _) =>
+    case Event(Status.Failure(e), _) =>
       log.error(e, "Connection interrupted")
       goto(Inactive)
-    case Event(OnNext(msg: TextMessage), _) =>
+    case event @ Event(_: WsMessage[_], _) => handleWsMessages(event)
+    case Event(msg: TextMessage, _) =>
       msg.textStream.runWith(Sink.fold("")(_ + _)).foreach { payload =>
         log.debug(s"Received payload:\n$payload")
         parser.parse(payload).flatMap(_.as[WsMessage[_]]) match {
@@ -149,50 +160,6 @@ class WsHandler(token: String, cache: ActorRef, settings: DiscordClientSettings)
       }
 
       stay
-    case Event(Hello(data), WithSource(source, resume)) =>
-      val payload = resume match {
-        case Some(resumeData) => (Resume(resumeData): WsMessage[ResumeData]).asJson.noSpaces
-        case None =>
-          (Identify(
-            IdentifyObject(
-              token,
-              IdentifyObject.createProperties,
-              compress = false,
-              settings.largeThreshold,
-              Seq(settings.shardNum, settings.shardTotal)
-            )
-          ): WsMessage[IdentifyObject]).asJson.noSpaces
-      }
-      log.debug(s"Sending payload: $payload")
-      source.offer(TextMessage(payload))
-      val cancellable = system.scheduler.schedule(0 seconds, data.heartbeatInterval millis, self, SendHeartbeat)
-      stay using WithHeartbeat(data.heartbeatInterval, cancellable, receivedAck = true, source, resume)
-    case Event(dispatch: Dispatch[d], data: WithHeartbeat) =>
-      val seq = dispatch.sequence
-      val event = dispatch.event
-      val d = dispatch.d
-
-      val updatedResume = data.resume.map(_.copy(seq = seq))
-      val updatedData   = data.copy(resume = updatedResume)
-
-      val stayRes = event match {
-        case WsEvent.Ready =>
-          val readyData = d.asInstanceOf[ReadyData]
-          log.debug("Ready trace:")
-          readyData._trace.foreach(log.debug)
-          val resumeData = ResumeData(token, readyData.sessionId, seq)
-
-          stay using data.copy(resume = Some(resumeData))
-        case _ =>
-          stay using updatedData
-      }
-
-      cache ! APIMessageHandlerEvent(d, event.createEvent)(event.handler)
-
-      stayRes
-    case Event(HeartbeatACK(_), data: WithHeartbeat) =>
-      log.debug("Received HeartbeatACK")
-      stay using data.copy(receivedAck = true)
     case Event(SendHeartbeat, data @ WithHeartbeat(_, _, receivedAck, source, resume)) =>
       if (!receivedAck) {
         log.error("Did not receive a Heartbeat ACK between heartbeats, reconnecting")
@@ -207,6 +174,52 @@ class WsHandler(token: String, cache: ActorRef, settings: DiscordClientSettings)
 
         stay using data.copy(receivedAck = false)
       }
+    case Event(ShutdownClient, _) =>
+      goto(ShuttingDown)
+  }
+
+  def handleWsMessages: StateFunction = {
+    case Event(Hello(data), WithSource(source, resume)) =>
+      val payload = resume match {
+        case Some(resumeData) => (Resume(resumeData): WsMessage[ResumeData]).asJson.noSpaces
+        case None =>
+          val identifyObject = IdentifyObject(
+            token = token,
+            properties = IdentifyObject.createProperties,
+            compress = false,
+            largeThreshold = settings.largeThreshold,
+            shard = Seq(settings.shardNum, settings.shardTotal)
+          )
+
+          (Identify(identifyObject): WsMessage[IdentifyObject]).asJson.noSpaces
+      }
+
+      log.debug(s"Sending payload: $payload")
+      source.offer(TextMessage(payload))
+      val cancellable = system.scheduler.schedule(0.seconds, data.heartbeatInterval.millis, self, SendHeartbeat)
+      stay using WithHeartbeat(data.heartbeatInterval, cancellable, receivedAck = true, source, resume)
+    case Event(dispatch: Dispatch[_], data: WithHeartbeat) =>
+      val seq = dispatch.sequence
+      val event = dispatch.event
+      val d = dispatch.d
+
+      val stayData = event match {
+        case WsEvent.Ready =>
+          val readyData = d.asInstanceOf[ReadyData]
+          log.debug("Ready trace:")
+          readyData._trace.foreach(log.debug)
+          val resumeData = ResumeData(token, readyData.sessionId, seq)
+
+          data.copy(resume = Some(resumeData))
+        case _ =>
+          data.copy(resume = data.resume.map(_.copy(seq = seq)))
+      }
+
+      cache ! APIMessageHandlerEvent(d, event.createEvent)(event.handler)
+      stay using stayData
+    case Event(HeartbeatACK(_), data: WithHeartbeat) =>
+      log.debug("Received HeartbeatACK")
+      stay using data.copy(receivedAck = true)
     case Event(Reconnect, data) =>
       log.info("Was told to reconnect by gateway")
       goto(Inactive) using WithResumeData(data.resume)
@@ -215,16 +228,13 @@ class WsHandler(token: String, cache: ActorRef, settings: DiscordClientSettings)
       goto(Inactive) using WithResumeData(None)
 
     //External messages
-    case Event(request: RequestGuildMembers, WithHeartbeat(_, _, _, source, _)) =>
+    case Event(Request(request: RequestGuildMembers, requestCtx /*TODO: Find a way to send back the ctx here*/), WithHeartbeat(_, _, _, source, _)) =>
       val payload = (request: WsMessage[RequestGuildMembersData]).asJson.noSpaces
       log.debug(s"Sending payload: $payload")
       source.offer(TextMessage(payload))
       log.debug("Requested guild data for {}", request.d.guildId)
 
       stay
-    case Event(ShutdownClient, _) =>
-      shouldShutDown = true
-      goto(Inactive)
   }
 
   initialize()
@@ -232,14 +242,18 @@ class WsHandler(token: String, cache: ActorRef, settings: DiscordClientSettings)
 object WsHandler {
   def props(token: String, cache: ActorRef, settings: DiscordClientSettings): Props = Props(classOf[WsHandler], token, cache, settings)
 
-  case object TryToConnect
-  case class ReceivedGateway(uri: Uri)
-  case object SendHeartbeat
-  case object ValidWsUpgrade
+  private case object TryToConnect
+  private case class ReceivedGateway(uri: Uri)
+  private case object SendHeartbeat
+  private case object ValidWsUpgrade
+  private case object ShutdownStart
+  private case object ShutdownFinish
+  private case object QueueCompleted
 
   sealed trait State
-  case object Inactive extends State
-  case object Active   extends State
+  case object Inactive     extends State
+  case object Active       extends State
+  case object ShuttingDown extends State
 
   sealed trait Data {
     def resume:                 Option[ResumeData]
