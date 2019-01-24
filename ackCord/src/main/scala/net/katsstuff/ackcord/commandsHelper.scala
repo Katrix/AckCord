bc8c81a3a8ce6552eb341cc1309ef03bc8dcd2c8
Katrix <katrix97@hotmail.com>
2019-01-24 15:03:53 +0100

    It's now 2019

--- ackCord/src/main/scala/net/katsstuff/ackcord/commandsHelper.scala
+++ ackCord/src/main/scala/net/katsstuff/ackcord/commandsHelper.scala
@@ -1,7 +1,7 @@
 /*
  * This file is part of AckCord, licensed under the MIT License (MIT).
  *
- * Copyright (c) 2018 Katrix
+ * Copyright (c) 2019 Katrix
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
