4a0f26a666935e9d7303bbd65b0ab67eabf15974
Katrix <katrix97@hotmail.com>
2019-12-03 18:36:59 +0100

    Fix ShardShutdownManager

--- ackCord/src/main/scala/ackcord/ShardShutdownManager.scala
+++ ackCord/src/main/scala/ackcord/ShardShutdownManager.scala
@@ -32,17 +32,21 @@ object ShardShutdownManager {
     Behaviors
       .receive[DiscordShard.StopShard.type] {
         case (ctx, DiscordShard.StopShard) =>
-          shards.foreach { shard =>
-            ctx.watch(shard)
-            shard ! DiscordShard.StopShard
-          }
+          if (shards.isEmpty) {
+            Behaviors.stopped
+          } else {
+            shards.foreach { shard =>
+              ctx.watch(shard)
+              shard ! DiscordShard.StopShard
+            }
 
-          Behaviors.same
+            Behaviors.same
+          }
       }
       .receiveSignal {
         case (_, Terminated(deadShard)) =>
           val newShards = shards.filter(_ != deadShard)
-          if (shards.isEmpty) {
+          if (newShards.isEmpty) {
             Behaviors.stopped
           } else {
             apply(newShards)
