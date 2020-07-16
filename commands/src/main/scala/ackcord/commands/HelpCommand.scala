5c35ef36b43bdd0ba8980903446bf46567205ec3
Katrix <katrix97@hotmail.com>
2020-07-16 19:08:42 +0200

    Add a help command system to the new commands

---
+++ commands/src/main/scala/ackcord/commands/HelpCommand.scala
@@ -0,0 +1,144 @@
+/*
+ * This file is part of AckCord, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2019 Katrix
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package ackcord.commands
+
+import java.util.concurrent.ConcurrentHashMap
+
+import scala.concurrent.Future
+import scala.collection.JavaConverters._
+import scala.collection.mutable
+
+import ackcord.CacheSnapshot
+import ackcord.data.Message
+import ackcord.requests.{CreateMessage, CreateMessageData, Requests}
+import akka.{Done, NotUsed}
+import cats.data.OptionT
+import cats.instances.future._
+import cats.syntax.all._
+
+/**
+  * The basic structure for a help command. Only accepts commands that use
+  * [[StructuredPrefixParser]].
+  */
+abstract class HelpCommand(requests: Requests) extends CommandController(requests) {
+  import requests.system.executionContext
+
+  protected val registeredCommands: mutable.Set[HelpCommand.HelpCommandEntry] =
+    ConcurrentHashMap.newKeySet[HelpCommand.HelpCommandEntry]().asScala
+
+  val help: ComplexCommand[Option[HelpCommand.Args], NotUsed] =
+    Command.parsing[Option[HelpCommand.Args]].asyncOptRequest { implicit m =>
+      m.parsed match {
+        case Some(HelpCommand.Args.CommandArgs(query)) =>
+          OptionT
+            .liftF(filterCommands(query))
+            .map(matches => CreateMessage(m.textChannel.id, createSearchReply(m.message, query, matches)))
+        case Some(HelpCommand.Args.PageArgs(page)) =>
+          OptionT.some[Future](CreateMessage(m.textChannel.id, createReplyAll(m.message, page)))
+        case None =>
+          OptionT.some[Future](CreateMessage(m.textChannel.id, createReplyAll(m.message, 0)))
+      }
+    }
+
+  def registerCommand(prefix: StructuredPrefixParser, description: CommandDescription, done: Future[Done]): Unit = {
+    val entry = HelpCommand.HelpCommandEntry(prefix, description)
+    registeredCommands.add(entry)
+
+    done.foreach(_ => removeEntry(entry))
+  }
+
+  protected def filterCommands(
+      query: String
+  )(implicit m: UserCommandMessage[_]): Future[Seq[HelpCommand.HelpCommandProcessedEntry]] = {
+    Future
+      .traverse(registeredCommands)(entry => entry.prefixParser.symbols(m.cache, m.message).map(entry -> _))
+      .flatMap { withSymbols =>
+        val entriesWithSymbolMatch = withSymbols.filter(_._2.exists(query.startsWith))
+        Future.traverse(entriesWithSymbolMatch) {
+          case (entry, symbols) =>
+            entry.prefixParser.aliases(m.cache, m.message).map(aliases => (entry, symbols, aliases))
+        }
+      }
+      .flatMap { withAliases =>
+        val entriesWithAliasMatch = withAliases.filter {
+          case (_, symbols, aliases) =>
+            val symbolsWithLength = symbols.map(_.length)
+            aliases.exists(alias => symbolsWithLength.exists(query.startsWith(alias, _)))
+        }
+        Future.traverse(entriesWithAliasMatch) {
+          case (entry, symbols, aliases) =>
+            entry.prefixParser
+              .needsMention(m.cache, m.message)
+              .zipWith(entry.prefixParser.caseSensitive(m.cache, m.message)) { (needsMention, caseSensitive) =>
+                HelpCommand.HelpCommandProcessedEntry(needsMention, symbols, aliases, caseSensitive, entry.description)
+              }
+
+        }
+      }
+      .map(_.toSeq)
+  }
+
+  protected def removeEntry(entry: HelpCommand.HelpCommandEntry): Unit =
+    registeredCommands.remove(entry)
+
+  /**
+    * Create a reply for a search result
+    * @param matches All the commands that matched the arguments
+    * @return Data to create a message describing the search
+    */
+  def createSearchReply(message: Message, query: String, matches: Seq[HelpCommand.HelpCommandProcessedEntry])(
+      implicit c: CacheSnapshot
+  ): CreateMessageData
+
+  /**
+    * Create a reply for all the commands tracked by this help command.
+    * @param page The page to use. Starts at 0.
+    * @return Data to create a message describing the commands tracked
+    *         by this help command.
+    */
+  def createReplyAll(message: Message, page: Int)(implicit c: CacheSnapshot): CreateMessageData
+
+  def unknownCmd(command: String): Option[CreateMessageData] =
+    Some(CreateMessageData(s"Unknown command $command"))
+}
+object HelpCommand {
+  case class HelpCommandEntry(prefixParser: StructuredPrefixParser, description: CommandDescription)
+  case class HelpCommandProcessedEntry(
+      needsMention: Boolean,
+      symbols: Seq[String],
+      aliases: Seq[String],
+      caseSensitive: Boolean,
+      description: CommandDescription
+  )
+
+  sealed trait Args
+  object Args {
+    case class CommandArgs(command: String) extends Args
+    case class PageArgs(page: Int)          extends Args
+
+    //We write out the parser ourself as string parses any string
+    implicit val parser: MessageParser[Args] =
+      MessageParser.intParser.map(PageArgs).orElse(MessageParser.stringParser.map(CommandArgs)).map(_.merge)
+  }
+}
