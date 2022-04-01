14b44e51e2ea5124f3f725ac46ae6f52bccccdef
Daave <root@widgetbot.io>
2022-04-01 22:54:44 +0100

    feat: Add test commands for new dropdowns

--- example/src/main/scala/ackcord/example/MyComponentCommands.scala
+++ example/src/main/scala/ackcord/example/MyComponentCommands.scala
@@ -2,7 +2,7 @@ package ackcord.example
 
 import ackcord.Requests
 import ackcord.commands.{CommandController, NamedDescribedCommand, UserCommandMessage}
-import ackcord.data.{ActionRow, Button, SelectOption, StringSelect}
+import ackcord.data._
 import ackcord.interactions.components._
 import ackcord.interactions.{ComponentInteraction, InteractionResponse, MenuInteraction}
 import ackcord.syntax._
@@ -72,5 +72,121 @@ class MyComponentCommands(requests: Requests) extends CommandController(requests
     )
   }
 
-  val commands = Seq(hiButton, singleSelect, multiSelect)
+  val singleRoleSelect: NamedDescribedCommand[NotUsed] = componentCommand("makeSingleRoleSelect") { _ =>
+    Seq(
+      ActionRow.of(
+        RoleSelect().onSelect(new AutoMenuHandler(_, requests) {
+          override def handle(implicit interaction: MenuInteraction): InteractionResponse = {
+            println(interaction.resolved)
+            sendMessage(s"Selected: ${interaction.values.mkString(", ")}")
+          }
+        })
+      )
+    )
+  }
+
+  val multiRoleSelect: NamedDescribedCommand[NotUsed] = componentCommand("makeMultiRoleSelect") { _ =>
+    Seq(
+      ActionRow.of(
+        RoleSelect(
+          minValues = 0,
+          maxValues = 3
+        ).onSelect(new AutoMenuHandler(_, requests) {
+          override def handle(implicit interaction: MenuInteraction): InteractionResponse = {
+            println(interaction.resolved)
+            sendMessage(s"Selected: ${interaction.values.mkString(", ")}")
+          }
+        })
+      )
+    )
+  }
+
+  val singleChannelSelect: NamedDescribedCommand[NotUsed] = componentCommand("makeSingleChannelSelect") { _ =>
+    Seq(
+      ActionRow.of(
+        ChannelSelect().onSelect(new AutoMenuHandler(_, requests) {
+          override def handle(implicit interaction: MenuInteraction): InteractionResponse =
+            sendMessage(s"Selected: ${interaction.values.mkString(", ")}")
+        })
+      )
+    )
+  }
+
+  val multiChannelSelect: NamedDescribedCommand[NotUsed] = componentCommand("makeMultiChannelSelect") { _ =>
+    Seq(
+      ActionRow.of(
+        ChannelSelect(
+          minValues = 0,
+          maxValues = 3
+        ).onSelect(new AutoMenuHandler(_, requests) {
+          override def handle(implicit interaction: MenuInteraction): InteractionResponse =
+            sendMessage(s"Selected: ${interaction.values.mkString(", ")}")
+        })
+      )
+    )
+  }
+
+  val singleUserSelect: NamedDescribedCommand[NotUsed] = componentCommand("makeSingleUserSelect") { _ =>
+    Seq(
+      ActionRow.of(
+        UserSelect().onSelect(new AutoMenuHandler(_, requests) {
+          override def handle(implicit interaction: MenuInteraction): InteractionResponse =
+            sendMessage(s"Selected: ${interaction.values.mkString(", ")}")
+        })
+      )
+    )
+  }
+
+  val multiUserSelect: NamedDescribedCommand[NotUsed] = componentCommand("makeMultiUserSelect") { _ =>
+    Seq(
+      ActionRow.of(
+        UserSelect(
+          minValues = 0,
+          maxValues = 3
+        ).onSelect(new AutoMenuHandler(_, requests) {
+          override def handle(implicit interaction: MenuInteraction): InteractionResponse =
+            sendMessage(s"Selected: ${interaction.values.mkString(", ")}")
+        })
+      )
+    )
+  }
+
+  val singleMentionableSelect: NamedDescribedCommand[NotUsed] = componentCommand("makeSingleMentionableSelect") { _ =>
+    Seq(
+      ActionRow.of(
+        MentionableSelect().onSelect(new AutoMenuHandler(_, requests) {
+          override def handle(implicit interaction: MenuInteraction): InteractionResponse =
+            sendMessage(s"Selected: ${interaction.values.mkString(", ")}")
+        })
+      )
+    )
+  }
+
+  val multiMentionableSelect: NamedDescribedCommand[NotUsed] = componentCommand("makeMultiMentionableSelect") { _ =>
+    Seq(
+      ActionRow.of(
+        MentionableSelect(
+          minValues = 0,
+          maxValues = 3
+        ).onSelect(new AutoMenuHandler(_, requests) {
+          override def handle(implicit interaction: MenuInteraction): InteractionResponse =
+            sendMessage(s"Selected: ${interaction.values.mkString(", ")}")
+        })
+      )
+    )
+  }
+
+  val commands = Seq(
+    hiButton,
+    singleSelect,
+    multiSelect,
+    singleRoleSelect,
+    multiRoleSelect,
+    singleChannelSelect,
+    multiChannelSelect,
+    singleUserSelect,
+    multiUserSelect,
+    singleMentionableSelect,
+    multiMentionableSelect
+  )
 }
