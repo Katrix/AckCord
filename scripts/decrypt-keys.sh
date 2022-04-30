c1f37f54f53a0adfa009cf30dcd5f02900bd69de
Katrix <katrix97@hotmail.com>
2022-04-30 20:59:00 +0200

    Enable building of the microsite when a new version is released

--- scripts/decrypt-keys.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-openssl aes-256-cbc -K $encrypted_4e8862369e3e_key -iv $encrypted_4e8862369e3e_iv -in travis-deploy-key.enc -out travis-deploy-key -d;
-chmod 600 travis-deploy-key;
-cp travis-deploy-key ~/.ssh/id_rsa;
--- scripts/publishMicrosite.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-git config --global user.email "katrix97@hotmail.com"
-git config --global user.name "Katrix"
-git config --global push.default simple
-
-sbt docs/publishMicrosite
